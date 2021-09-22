2180cfda36cbaa8933a51b7db9766a580e2ac63d
ThePotatoDev <potatocoding@gmail.com>
2021-09-21 22:23:39 -0500

    move gson instance to plugin class

--- src/main/java/gg/tater/bedrock/BedrockPlugin.java
+++ src/main/java/gg/tater/bedrock/BedrockPlugin.java
@@ -1,7 +1,13 @@
 package gg.tater.bedrock;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import gg.tater.bedrock.adapter.ItemStackAdapter;
+import gg.tater.bedrock.adapter.LocationAdapter;
 import gg.tater.bedrock.database.BedrockDatabase;
 import gg.tater.bedrock.database.Credentials;
+import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Optional;
@@ -10,6 +16,12 @@ import java.util.concurrent.atomic.AtomicReference;
 public class BedrockPlugin extends JavaPlugin {
 
     private static final AtomicReference<BedrockDatabase> DATABASE_REFERENCE = new AtomicReference<>();
+
+    public static Gson SHARED_GSON = new GsonBuilder()
+            .registerTypeAdapter(ItemStack.class, new ItemStackAdapter())
+            .registerTypeAdapter(Location.class, new LocationAdapter())
+            .serializeNulls()
+            .create();
 
     @Override
     public void onEnable() {
